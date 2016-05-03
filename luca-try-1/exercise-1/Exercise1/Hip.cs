4c70a016cafdf5542136c7a957f3199c88a3caf9
Luca Minudel <lucaminudel@Lucas-MacBook-Air.local>
2016-05-03 23:38:41 +0100

    Minor refactoring to reduce code verbosity.

--- luca-try-1/exercise-1/Exercise1/Hip.cs
+++ luca-try-1/exercise-1/Exercise1/Hip.cs
@@ -3,22 +3,18 @@ namespace Exercise1
 {
     public class Hip
     {
-        private readonly RightThigh _rightThigh; 
-        private readonly RightLowerLimb _rightLowerLimb;
-        private readonly RightFoot _rightFoot;
-
-        public RightThigh RightThigh { get { return _rightThigh; } }
-        public RightLowerLimb RightLowerLimb { get { return _rightLowerLimb; } }
-        public RightFoot RightFoot { get { return _rightFoot; } }
+        public RightThigh RightThigh { get; private set; }
+        public RightLowerLimb RightLowerLimb { get; private set; }
+        public RightFoot RightFoot { get; private set; }
 
         public int MaxCharge { get { return 300; } }
         public bool IsOn { get { return true; } }
 
         public Hip()
         {
-            _rightThigh = new RightThigh();
-            _rightLowerLimb = new RightLowerLimb();
-            _rightFoot = new RightFoot();
+            RightThigh = new RightThigh();
+            RightLowerLimb = new RightLowerLimb();
+            RightFoot = new RightFoot();
         }
 
         // Other Hip respnonsibilities omitted because implementation is not needed for the exercise
--- luca-try-1/exercise-1/Exercise1/RobotBus.cs
+++ luca-try-1/exercise-1/Exercise1/RobotBus.cs
@@ -4,25 +4,19 @@ namespace Exercise1
 {
     public class RobotBus
     {
-        private readonly LeftArm _leftArm;
-        private readonly Neck _neck;
-        private readonly Head _head;
-
-        private readonly Hip _hip;
-
         public bool IsOn { get { return true; } }
-        public LeftArm LeftArm { get { return _leftArm;  } }
-        public Neck Neck { get { return _neck; } }
-        public Head Head { get { return _head; } }
-        public Hip Hip { get { return _hip; } }
+        public LeftArm LeftArm { get; private set; }
+        public Neck Neck { get; private set; }
+        public Head Head { get; private set; }
+        public Hip Hip { get; private set; }
 
         public RobotBus()
         {
-            _leftArm = new LeftArm();
-            _neck = new Neck();
-            _head = new Head();
+            LeftArm = new LeftArm();
+            Neck = new Neck();
+            Head = new Head();
 
-            _hip = new Hip();
+            Hip = new Hip();
         }
 
         // Other RobotBus respnonsibilities omitted because implementation is not needed for the exercise
